e2a634e0b8858134b1c36525b7c77f0dcb8b4cef
Pedro Vilela <46492888+phvilela87@users.noreply.github.com>
2021-08-22 16:49:30 -0300

    Creating project

---
+++ CampeoesDaCopa/CampeoesDaCopa/Controllers/WorldCupViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WorldCupViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Pedro Vilela on 22/08/21.
+//
+
+import UIKit
+
+class WorldCupViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
---
+++ CampeoesDaCopa/CampeoesDaCopa/Views/WinnerTableViewController.swift
@@ -0,0 +1,8 @@
+//
+//  WinnerTableViewController.swift
+//  CampeoesDaCopa
+//
+//  Created by Pedro Vilela on 22/08/21.
+//
+
+import Foundation
