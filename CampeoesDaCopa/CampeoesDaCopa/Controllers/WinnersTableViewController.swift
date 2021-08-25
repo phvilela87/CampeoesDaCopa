9b6af69aa7915b6f63176277e4e3e87d8c8a0997
Pedro Vilela <phvilela87@gmail.com>
2021-08-24 23:02:28 -0300

    Implementing customised table view cells

--- CampeoesDaCopa/CampeoesDaCopa/Controllers/WinnersTableViewController.swift
+++ CampeoesDaCopa/CampeoesDaCopa/Controllers/WinnersTableViewController.swift
@@ -23,6 +23,11 @@ class WinnersTableViewController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! WorldCupViewController
+        let worldCup = worldCups[tableView.indexPathForSelectedRow!.row]
+        vc.worldCup = worldCup
+    }
     
     func loadWorldCups() {
         let fileURL = Bundle.main.url(forResource: "winners", withExtension: "json")!
@@ -47,13 +52,11 @@ class WinnersTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! WorldCupTableViewCell
 
         let worldCup = worldCups[indexPath.row]
-        cell.textLabel?.text = "Copa \(worldCup.year) - \(worldCup.country)"
-        cell.detailTextLabel?.text = "\(worldCup.winner) vs \(worldCup.vice))"
-        cell.imageView?.image = UIImage(named: "\(worldCup.winner).png")
-        
+        cell.prepare(with: worldCup)
+
         return cell
     }
 
--- CampeoesDaCopa/CampeoesDaCopa/Controllers/WorldCupViewController.swift
+++ CampeoesDaCopa/CampeoesDaCopa/Controllers/WorldCupViewController.swift
@@ -9,8 +9,12 @@ import UIKit
 
 class WorldCupViewController: UIViewController {
 
+    var worldCup: WorldCup!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "WorldCup \(worldCup.year)"
     }
     
     
